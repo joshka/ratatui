aef7aa1884d3ad64bb703a5826fe7ce6dc1ec35a
Josh McKinney <joshka@users.noreply.github.com>
2024-01-14 18:47:53 -0800

    style: tweak colors for accessibility

--- examples/constraints.rs
+++ examples/constraints.rs
@@ -26,7 +26,7 @@ const LENGTH_COLOR: Color = tailwind::TEAL.c950;
 const PERCENTAGE_COLOR: Color = tailwind::TEAL.c900;
 const RATIO_COLOR: Color = tailwind::TEAL.c700;
 // priority 4
-const PROPORTIONAL_COLOR: Color = tailwind::YELLOW.c950;
+const PROPORTIONAL_COLOR: Color = tailwind::YELLOW.c400;
 
 fn main() -> Result<(), Box<dyn Error>> {
     // setup terminal
