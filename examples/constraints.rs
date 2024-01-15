dbf8f23b46753099c5d9760cbe9c32772e860dac
Josh McKinney <joshka@users.noreply.github.com>
2024-01-14 18:55:42 -0800

    style: tweak colors for accessibility3

--- examples/constraints.rs
+++ examples/constraints.rs
@@ -19,8 +19,8 @@ const EXAMPLE_HEIGHT: u16 = ILLUSTRATION_HEIGHT + SPACER_HEIGHT;
 // priority 1
 const FIXED_COLOR: Color = tailwind::RED.c900;
 // priority 2
-const MIN_COLOR: Color = tailwind::INDIGO.c900;
-const MAX_COLOR: Color = tailwind::INDIGO.c700;
+const MIN_COLOR: Color = tailwind::BLUE.c900;
+const MAX_COLOR: Color = tailwind::BLUE.c700;
 // priority 3
 const LENGTH_COLOR: Color = tailwind::TEAL.c950;
 const PERCENTAGE_COLOR: Color = tailwind::TEAL.c900;
